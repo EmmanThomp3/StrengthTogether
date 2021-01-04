bf1ba8e1fbfb50157701f62526186290de859782
Adokiye <gorgeceo@gmail.com>
2021-01-04 08:30:56 +0100

    fix for null array

--- lib/Screens/home/home.dart
+++ lib/Screens/home/home.dart
@@ -17,7 +17,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamProvider<DocumentSnapshot>.value(
-      value: DatabaseService(uid:_auth.currentUser.uid).specificUserData,
+      value: DatabaseService(uid: _auth.currentUser.uid).specificUserData,
       child: HomeChild(),
     );
   }
@@ -26,69 +26,75 @@ class Home extends StatelessWidget {
 class HomeChild extends StatelessWidget {
   final AuthService _auth = AuthService();
   final FirebaseAuth _uid = FirebaseAuth.instance;
-  void customLaunch(command) async{
-    if(await canLaunch(command)){
+  void customLaunch(command) async {
+    if (await canLaunch(command)) {
       await launch(command);
       print(_uid.currentUser.uid);
-    }else{
+    } else {
       print("could not launch $command");
       print(_uid.currentUser.uid);
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    if(Provider.of<DocumentSnapshot>(context) != null){
-      if(Provider.of<DocumentSnapshot>(context).data()['active'] != null&&Provider.of<DocumentSnapshot>(context).data()['active']){
-    return WebviewScaffold(
-      appBar: AppBar(
-        title: Text(
-          'ST',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.yellow,
-            fontSize: 40,
-          ),
-        ),
-        backgroundColor: Colors.black,
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton(onPressed: (){
-            customLaunch('tel:+1 708 510 9397');
-          }, child: Text('Panic',
-            style: TextStyle(color: Colors.red,
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          )),
-          FlatButton.icon(
-            icon: Icon(
-              Icons.person,
-              color: Colors.yellow,
-              size: 20,
-            ),
-            label: Text(
-              'Logout',
+    if (Provider.of<DocumentSnapshot>(context) != null && Provider.of<DocumentSnapshot>(context).data() != null) {
+      if (Provider.of<DocumentSnapshot>(context).data()['active'] != null &&
+          Provider.of<DocumentSnapshot>(context).data()['active']) {
+        return WebviewScaffold(
+          appBar: AppBar(
+            title: Text(
+              'ST',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Colors.yellow,
-                fontSize: 20,
+                fontSize: 40,
               ),
             ),
-            onPressed: () async {
-              await _auth.signOut();
-            },
-          )
-        ],
-      ),
-      url:
-      "https://webchat.botframework.com/embed/STAPPBOT?s=T3d4crx0NCU.5p8D_mrHFkZdqRjhWTdCqvCMWcHLY1cZq4vYt5IRcm8&userid=" + _uid.currentUser.uid,
-    );
-      }else{
-       return SessionCompleted();
+            backgroundColor: Colors.black,
+            elevation: 0.0,
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    customLaunch('tel:+1 708 510 9397');
+                  },
+                  child: Text(
+                    'Panic',
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+              FlatButton.icon(
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.yellow,
+                  size: 20,
+                ),
+                label: Text(
+                  'Logout',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.yellow,
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+              )
+            ],
+          ),
+          url:
+              "https://webchat.botframework.com/embed/STAPPBOT?s=T3d4crx0NCU.5p8D_mrHFkZdqRjhWTdCqvCMWcHLY1cZq4vYt5IRcm8&userid=" +
+                  _uid.currentUser.uid,
+        );
+      } else {
+        return SessionCompleted();
       }
-    }else{
+    } else {
       return Container();
     }
-
   }
 }
